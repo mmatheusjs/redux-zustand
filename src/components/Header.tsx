e7eb6d7d843a12c31dfa61ff3aec3ee665091cab
mmatheusjs <matheus.oliveira@cazco.digital>
2024-08-05 16:45:17 -0300

    Configuration of player slice

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,21 @@
+import { useAppSelector } from "../store";
+
 export function Header() {
-  return (
-    <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold"> Fundamentos do Redux</h1>
-      <span className="text-small text-zinc-400">
-        Módulo "Desvendando o Redux"
-      </span>
-    </div>
-  );
+	const { currentModule, currentLesson } = useAppSelector((state) => {
+		const { currentModuleIndex, currentLessonIndex } = state.player;
+
+		const currentModule = state.player.course.modules[currentModuleIndex];
+		const currentLesson = currentModule.lessons[currentLessonIndex];
+
+		return { currentLesson, currentModule };
+	});
+
+	return (
+		<div className="flex flex-col gap-1">
+			<h1 className="text-2xl font-bold"> {currentLesson.title}</h1>
+			<span className="text-small text-zinc-400">
+				Módulo "{currentModule.title}"
+			</span>
+		</div>
+	);
 }
--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,33 +1,60 @@
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 import * as Collapsible from "@radix-ui/react-collapsible";
+import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player";
 
 interface ModuleProps {
-  moduleIndex: number;
-  title: string;
-  amountOfLessons: number;
+	moduleIndex: number;
+	title: string;
+	amountOfLessons: number;
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  return (
-    <Collapsible.Root className="group">
-      <Collapsible.Trigger className=" flex w-full items-center gap-3 bg-zinc-800 p-4">
-        <span className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-          {moduleIndex + 1}
-        </span>
-        <div className="flex flex-col gap-1 text-left">
-          <strong className="text-sm">{title}</strong>
-          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
-        </div>
-        <ChevronDown className="W-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
-      </Collapsible.Trigger>
-      <Collapsible.Content>
-        <nav className="relative flex flex-col gap-4 p-6">
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
-          <Lesson title="Fundamentos do Redux" duration="09:13" />
-        </nav>
-      </Collapsible.Content>
-    </Collapsible.Root>
-  );
+	const dispatch = useDispatch();
+
+	const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
+		const { currentModuleIndex, currentLessonIndex } = state.player;
+
+		return { currentModuleIndex, currentLessonIndex };
+	});
+
+	const lessons = useAppSelector((state) => {
+		return state.player.course.modules[moduleIndex].lessons;
+	});
+
+	console.log("lessons", lessons);
+	return (
+		<Collapsible.Root className="group">
+			<Collapsible.Trigger className=" flex w-full items-center gap-3 bg-zinc-800 p-4">
+				<span className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
+					{moduleIndex + 1}
+				</span>
+				<div className="flex flex-col gap-1 text-left">
+					<strong className="text-sm">{title}</strong>
+					<span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
+				</div>
+				<ChevronDown className="W-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+			</Collapsible.Trigger>
+			<Collapsible.Content>
+				<nav className="relative flex flex-col gap-4 p-6">
+					{lessons.map((lesson, lessonIndex) => {
+						const isCurrent =
+							currentModuleIndex === moduleIndex &&
+							currentLessonIndex === lessonIndex;
+						return (
+							<Lesson
+								key={lesson.id}
+								title={lesson.title}
+								duration={lesson.duration}
+								onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+								isCurrent={isCurrent}
+							/>
+						);
+					})}
+				</nav>
+			</Collapsible.Content>
+		</Collapsible.Root>
+	);
 }

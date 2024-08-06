99d2b9a87efbfc1376c289fdc61bbd841e24b5c0
mmatheusjs <matheus.oliveira@cazco.digital>
2024-08-06 18:17:55 -0300

    Progress on update store, creation of useCurrentLesson hook

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,24 +1,25 @@
 import ReactPlayer from "react-player";
-import { useAppSelector } from "../store";
+
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
-	const lesson = useAppSelector((state) => {
-		const { currentModuleIndex, currentLessonIndex } = state.player;
+	const dispatch = useDispatch();
 
-		const currentLesson =
-			state.player.course.modules[currentModuleIndex].lessons[
-				currentLessonIndex
-			];
+	const { currentLesson } = useCurrentLesson();
 
-		return currentLesson;
-	});
+	function handlePlayNext() {
+		dispatch(next());
+	}
 	return (
 		<div className="w-full bg-zinc-950 aspect-video">
 			<ReactPlayer
 				width={"100%"}
 				height={"100%"}
 				controls
-				url={`https://www.youtube.com/watch?v=${lesson.id}`}
+				playing
+				onEnded={handlePlayNext}
+				url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
 			/>
 		</div>
 	);
--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,6 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useAppSelector } from "..";
 
-const playerSlice = createSlice({
+export const playerSlice = createSlice({
 	name: "player",
 	initialState: {
 		course: {
@@ -61,12 +62,40 @@ const playerSlice = createSlice({
 		currentLessonIndex: 0,
 	},
 	reducers: {
-		play: (state, action) => {
+		play: (state, action: PayloadAction<[number, number]>) => {
 			state.currentModuleIndex = action.payload[0];
 			state.currentLessonIndex = action.payload[1];
+		},
+		next: (state) => {
+			const nextLessonIndex = state.currentLessonIndex + 1;
+			const nextLesson =
+				state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex];
+
+			if (nextLesson) {
+				state.currentLessonIndex = nextLessonIndex;
+			} else {
+				const nextModuleIndex = state.currentModuleIndex + 1;
+				const nextModule = state.course.modules[nextModuleIndex];
+
+				if (nextModule) {
+					state.currentModuleIndex = nextModuleIndex;
+					state.currentLessonIndex = 0;
+				}
+			}
 		},
 	},
 });
 
 export const player = playerSlice.reducer;
-export const { play } = playerSlice.actions;
+export const { play, next } = playerSlice.actions;
+
+export const useCurrentLesson = () => {
+	return useAppSelector((state) => {
+		const { currentModuleIndex, currentLessonIndex } = state.player;
+
+		const currentModule = state.player.course.modules[currentModuleIndex];
+		const currentLesson = currentModule.lessons[currentLessonIndex];
+
+		return { currentLesson, currentModule };
+	});
+};

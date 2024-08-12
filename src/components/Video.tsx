adbcf4e42a3d2783d071a020b80cbef304b3f82f
mmatheusjs <matheus.oliveira@cazco.digital>
2024-08-12 15:34:24 -0300

    ADd axions and json server

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -11,6 +11,11 @@ export function Video() {
 	function handlePlayNext() {
 		dispatch(next());
 	}
+
+	if (!currentLesson) {
+		return null;
+	}
+
 	return (
 		<div className="w-full bg-zinc-950 aspect-video">
 			<ReactPlayer

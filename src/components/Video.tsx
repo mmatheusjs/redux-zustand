58f0dae93f07852b2a8428b71ff6ea78bac18ca1
mmatheusjs <math.heenrique@live.com>
2024-08-19 09:05:59 -0300

    Add asyncThunk to fetch data using actions

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,31 +1,31 @@
 import ReactPlayer from "react-player";
 
-import { useDispatch } from "react-redux";
 import { next, useCurrentLesson } from "../store/slices/player";
+import { useAppDispatch } from "../store";
 
 export function Video() {
-	const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
 
-	const { currentLesson } = useCurrentLesson();
+  const { currentLesson } = useCurrentLesson();
 
-	function handlePlayNext() {
-		dispatch(next());
-	}
+  function handlePlayNext() {
+    dispatch(next());
+  }
 
-	if (!currentLesson) {
-		return null;
-	}
+  if (!currentLesson) {
+    return null;
+  }
 
-	return (
-		<div className="w-full bg-zinc-950 aspect-video">
-			<ReactPlayer
-				width={"100%"}
-				height={"100%"}
-				controls
-				playing
-				onEnded={handlePlayNext}
-				url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
-			/>
-		</div>
-	);
+  return (
+    <div className="w-full bg-zinc-950 aspect-video">
+      <ReactPlayer
+        width={"100%"}
+        height={"100%"}
+        controls
+        playing
+        onEnded={handlePlayNext}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
+      />
+    </div>
+  );
 }
